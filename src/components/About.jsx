299c17c4f1fa1e9e6ae0891d6890fa134a2e8b31
Garo San <garosanchez27@gmail.com>
2019-06-22 23:10:01 -0500

    Added presentational components and first hook

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,33 @@
 import React from "react";
 
-const About = () => <h1>Hello cool world!</h1>;
+const About = () => {
+  return (
+    <div className="About">
+      <div className="About-container">
+        <div className="About-avatar">
+          <figure>
+            <img src="/" alt="" />
+          </figure>
+        </div>
+        <div className="About-name">
+          <h2>Edgar Sánchez</h2>
+        </div>
+        <div className="About-profession">
+          <p>Frontend Developer</p>
+        </div>
+        <div className="About-desc">
+          <p>
+            Web developer with a passion for learning, improving and making a
+            positive impact.
+          </p>
+        </div>
+        <div className="About-location">
+          <p>Monterrey, MX</p>
+        </div>
+        <div className="About-social">Social</div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
---
+++ src/components/Experience.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="Experience">
+      <div className="Experience-container">
+        <div className="Experience-item">
+          <h3>MIT</h3>
+          <p>Aprendí un chingo.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
